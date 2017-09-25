d5e459875d9570d58ae05b41c0b0995dce11b011
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 16:54:06 -0400

    changed @earnings based on the quota

--- paperboy.rb
+++ paperboy.rb
@@ -1,21 +1,45 @@
 class PaperBoy
 
-  @name
-  @experience
-  @earnings #read-only
+  def initialize(name)
+    @name = name
+    @experience = 0 #number of papers delivered
+    @earnings = 0 #read-only
+  end
 
   def quota
     #calculate and return quota for next delivery
+    #min papers = 50
+    #quota 50% of experience + min papers
+    first_delivery = 50
+    @experience += (@experience * 0.50) + first_delivery
+    return @experience
   end
 
   def deliver(start_address, end_address)
     #2 house numbers needed - assume start smaller number than end
     #return amount of money earned as a .to_f
     #update experience
+    total_houses = (start_address..end_address).to_a
+    puts total_houses.length
+    total_houses.each do |address|
+      if address <= 50
+        @earnings += 0.25 # minimum earned = $12.5; adding 0.25 for every address delivered to
+      elsif address > 50
+        @earnings += 0.50
+      end
+    end
+
+    if total_houses.length < 50
+      @earnings -= 2.00
+    end
+
+    return @earnings
+
   end
 
   def report
     #return string of performance
+    puts "I'm #{@name}, I've delivered #{@experience} paperes and I've earned #{@earnings}"
   end
 
 end
@@ -27,3 +51,10 @@ end
 #min papers = 50
 #quota 50% of experience + min papers
   #eg. if quota of 50 met, next time quota = 75
+
+nick = PaperBoy.new("Nick")
+
+nick.quota
+nick.deliver(1, 70)
+
+puts nick.inspect
