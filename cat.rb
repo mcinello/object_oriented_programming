170027177e1f70585d9ea50b514411830f233ef1
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 13:30:46 -0400

    created 2 instances of the Cat class

--- cat.rb
+++ cat.rb
@@ -1,9 +1,15 @@
-class cat
+class Cat
 
-  def initialize
-    @name
-    @preferred_food
-    @meal_time
+  def initialize(name, food, time)
+    @name = name
+    @preferred_food = food
+    @meal_time = time
   end
 
 end
+
+cat1 = Cat.new("Howie", "fish", "12")
+cat2 = Cat.new("Oliver", "chicken", "14")
+
+puts cat1.inspect
+puts cat2.inspect
