5c0ca7365fd18a883cbed36bde1901286d29867d
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 13:10:42 -0400

    added 1000.00 to first_account @balance

--- ex1_bank-account.rb
+++ ex1_bank-account.rb
@@ -1,20 +1,30 @@
 class BankAccount
 
-@balance
-@interest_rate
-@amount
-
-
-  def deposit
-    @amount
+  def initialize(total)
+    @balance = total
+    # @interest_rate = interest
   end
 
-  def withdraw
-    @amount
+  def deposit(amount)
+    @balance += amount
+    return @balance
   end
 
-  def gain_interest
-
-  end
+  # def withdraw(amount)
+  #   @balance -= amount
+  # end
+  #
+  # def gain_interest(rate)
+  #   @interest_rate
+  # end
 
 end
+
+first_account = BankAccount.new(1000.00)
+
+puts first_account.inspect
+
+#
+#
+# first_account.deposit(100.00)
+# puts first_account.inspect
