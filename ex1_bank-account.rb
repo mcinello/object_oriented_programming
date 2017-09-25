aab871e6b103b4111244996695122ad3c68ccd49
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 13:21:56 -0400

    first_account gained an interest of 5.5%

--- ex1_bank-account.rb
+++ ex1_bank-account.rb
@@ -1,31 +1,36 @@
 class BankAccount
 
-  def initialize(total)
+  def initialize(total, interest)
     @balance = total
-    # @interest_rate = interest
+    @interest_rate = interest / 100
   end
 
-  def deposit(amount)
-    @balance += amount
-    return @balance
-  end
-
-  def withdraw(amount)
-    @balance -= amount
-    return @balance
-  end
-  #
-  # def gain_interest(rate)
-  #   @interest_rate
+  # def deposit(amount)
+  #   @balance += amount
+  #   return @balance
   # end
+  #
+  # def withdraw(amount)
+  #   @balance -= amount
+  #   return @balance
+  # end
+
+  def gain_interest
+    #increase balance according to interest rate
+    @balance += (@balance * @interest_rate)
+    return @balance
+  end
 
 end
 
-first_account = BankAccount.new(1000.00)
+first_account = BankAccount.new(1000.00, 5.5)
 puts first_account.inspect
 
-first_account.deposit(100.00)
+first_account.gain_interest
 puts first_account.inspect
 
-first_account.withdraw(500.50)
-puts first_account.inspect
+# first_account.deposit(100.00)
+# puts first_account.inspect
+
+# first_account.withdraw(500.50)
+# puts first_account.inspect
