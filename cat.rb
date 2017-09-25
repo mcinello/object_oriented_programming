32ae9895a97eb3d20875e2633352b74937a1fdea
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 13:34:29 -0400

    returned the values for the time the cat instances eat

--- cat.rb
+++ cat.rb
@@ -6,10 +6,20 @@ class Cat
     @meal_time = time
   end
 
+  def eats_at
+    puts "#{@name} eats at #{@meal_time}PM"
+  end
+
 end
 
 cat1 = Cat.new("Howie", "fish", "12")
-cat2 = Cat.new("Oliver", "chicken", "14")
+cat2 = Cat.new("Oliver", "chicken", "2")
 
 puts cat1.inspect
 puts cat2.inspect
+
+cat1.eats_at
+puts cat1
+
+cat2.eats_at
+puts cat2
