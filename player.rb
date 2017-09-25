a5845503def789d21f85a32923a9256fbf812cc8
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 13:58:44 -0400

    set up all instance methods

--- player.rb
+++ player.rb
@@ -1,26 +1,37 @@
 class Player
 
-  @gold_coins = 0
-  @health_points = 10
-  @lives = 5
+#starting points
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+  end
 
   def level_up
-    #increase lives by 1
+    @lives += 1
+    return @lives
   end
 
   def collect_treasure
-    #increases gold coins by 1
-    #if goold coins is a multiple of 10, then run the level_up method
+    @gold_coins += 1   #increases gold coins by 1
+    if @gold_coins % 10 == 0
+      Player.new.level_up #if gold coins is a multiple of 10, then run the level_up method
+    end
   end
 
   def do_battle(damage)
-    #subtracts from players health points
-    #if health_points fall below 1, subtract 1 from lives, reset health_points to 10
-    #if no more lives, method should run restart method
+    @health_points -= damage #subtracts from players health points
+    if @health_points < 1 #if health_points fall below 1, subtract 1 from lives, reset health_points to 10
+      @lives -= 1
+      Player.new.initialize.health_points #?? how to call specific attribute from method??
+    elsif @lives == 0
+      Player.new.restart #if no more lives, method should run restart method
+    end
   end
 
   def restart
-    #reset all attributes back to starting values
+    Player.new.initialize #reset all attributes back to starting values
+
   end
 
 end
