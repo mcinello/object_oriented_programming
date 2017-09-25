e9c147a19cf15ab694b0e394753f8fae21c420fa
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 14:49:08 -0400

    subtracted damage from health_points

--- player.rb
+++ player.rb
@@ -1,45 +1,55 @@
 class Player
 
-#starting points
   def initialize
     @gold_coins = 0
     @health_points = 10
     @lives = 5
   end
 
-  def level_up
+#answer 6
+  def level_up #works
     @lives += 1
     return @lives
   end
 
-  def collect_treasure
-    @gold_coins += 1   #increases gold coins by 1
+#answer 7
+  def collect_treasure #works
+    @gold_coins += 1
     if @gold_coins % 10 == 0
-      Player.new.level_up #if gold coins is a multiple of 10, then run the level_up method
+      Player.new.level_up
       return level_up
     end
   end
 
+#answer 8
   def do_battle(damage)
     @health_points -= damage #subtracts from players health points
-    if @health_points < 1 #if health_points fall below 1, subtract 1 from lives, reset health_points to 10
-      @lives -= 1
-      Player.new.initialize.health_points #?? how to call specific attribute from method??
-    elsif @lives == 0
-      Player.new.restart #if no more lives, method should run restart method
-    end
+    return @health_points
   end
 
-  def restart
-    Player.new.initialize #reset all attributes back to starting values
-  end
+  # def restart
+  #   Player.new #reset all attributes back to starting values
+  # end
 
 end
 
 player1 = Player.new
 puts player1.inspect
 
-20.times do
-  player1.collect_treasure
-  puts player1.inspect
-end
+# 20.times do
+#   player1.collect_treasure
+#   puts player1.inspect
+# end
+
+player1.do_battle(9)
+puts player1.inspect
+
+#   if @health_points < 1 #if health_points fall below 1, subtract 1 from lives, reset health_points to 10
+#       @lives -= 1
+#       @health_points = 10
+#       return health_points#?? how to call specific attribute from method??
+#       if @lives == 0
+#         Player.new.restart #if no more lives, method should run restart method
+#         return restart
+#       end
+#   end
