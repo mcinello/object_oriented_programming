8b81f942a404e596f9ed95fa4b0da6be105aa9d7
Michelle Cinello <m.cinello@gmail.com>
2017-09-25 15:11:33 -0400

    if lives == 0, all instance variables are  reset

--- player.rb
+++ player.rb
@@ -24,17 +24,24 @@ class Player
 #answer 8
   def do_battle(damage)
     @health_points -= damage #works
-    if @health_points < 1
+    if @health_points < 1 #works
       @lives -= 1
+      if @lives == 0
+        puts @lives
+        Player.new.restart
+        return restart
+      end
       @health_points = 10
-      return @lives, @health_points
+      return @health_points
     end
     return @health_points
   end
 
-  # def restart
-  #   Player.new #reset all attributes back to starting values
-  # end
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5 #reset all attributes back to starting values
+  end
 
 end
 
@@ -46,8 +53,10 @@ puts player1.inspect
 #   puts player1.inspect
 # end
 
-player1.do_battle(12)
-puts player1.inspect
+5.times do
+  player1.do_battle(10)
+  puts player1.inspect
+end
 
 #   if @health_points < 1 #if health_points fall below 1, subtract 1 from lives, reset health_points to 10
 #       @lives -= 1
